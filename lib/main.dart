9a6f708585f1bf420e17c996b9ac188c62e30c7c
Alex Serodio <alequis01@gmail.com>
2020-02-02 17:11:26 -0300

    add books mocks and update android app name and icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pretty_kindle_highlights/model/book.dart';
 import 'package:pretty_kindle_highlights/detail_page.dart';
+import 'package:pretty_kindle_highlights/mocks.dart';
 
 void main() => runApp(new MyApp());
 
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pretty Kindle Highlights',
       theme: new ThemeData(primaryColor: Color.fromRGBO(58, 66, 86, 1.0)),
       home: new ListPage(title: 'Books'),
     );
@@ -34,7 +35,7 @@ class _ListPageState extends State<ListPage> {
 
   @override
   void initState() {
-    books = getBooks();
+    books = Mocks.getBooks();
     super.initState();
   }
 
@@ -82,7 +83,7 @@ class _ListPageState extends State<ListPage> {
       child: ListView.builder(
         scrollDirection: Axis.vertical,
         shrinkWrap: true,
-        itemCount: 10,
+        itemCount: books.length,
         itemBuilder: (BuildContext context, int index) {
           return makeCard(books[index]);
         },
@@ -96,71 +97,4 @@ class _ListPageState extends State<ListPage> {
     );
   }
 
-}
-
-List getBooks() {
-  return [
-    Book(
-        title: "1984 (Classics To Go)",
-        author: "Orwell, George"
-    ),
-    Book(
-        title: "DevOps na prática: entrega de software confiável e automatizada",
-        author: "Danilo Sato"
-    ),
-    Book(
-        title: "O voto feminino no Brasil",
-        author: "Novaes Marques, Teresa Cristina"
-    ),
-    Book(
-        title: "Astrofísica Para Apressados",
-        author: "deGrasse Tyson, Neil"
-    ),
-    Book(
-        title: "Alice's Adventures in Wonderland (AmazonClassics Edition)",
-        author: "Carroll, Lewis"
-    ),
-    Book(
-        title: "O que é o amor?",
-        author: "Martins, Daniel"
-    ),
-    Book(
-        title: "Entre a Esquerda e a Direita: Uma reflexão política",
-        author: "Arrais, Rafael"
-    ),
-
-    Book(
-        title: "Sapiens",
-        author: "Harari, Yuval Noah"
-    ),
-    Book(
-        title: "Design Patterns com Java: Projeto orientado a objetos guiado por padrões",
-        author: "Eduardo Guerra"
-    ),
-    Book(
-        title: "Arrume a sua cama",
-        author: "Mcraven, William H."
-    ),
-    Book(
-        title: "Lean Inception: Como Alinhar Pessoas e Construir o Produto Certo",
-        author: "Caroli, Paulo"
-    ),
-    Book(
-        title: "Clube da luta",
-        author: "Palahniuk, Chuck"
-    ),
-
-    Book(
-        title: "O melhor do Cortella: Trilhas do Pensar - Ideias, Frases e Inspirações",
-        author: "Cortella, Mario Sergio"
-    ),
-    Book(
-        title: "A História do Universo para quem tem pressa",
-        author: "Stuart, Colin"
-    ),
-    Book(
-        title: "Minimalismo 2.0: Como simplificar sua vida no século XXI (e diminuir seu impacto no meio ambiente)",
-        author: "Müller, Izzy"
-    ),
-  ];
 }
